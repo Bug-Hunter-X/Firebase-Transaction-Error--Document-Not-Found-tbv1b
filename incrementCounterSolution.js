f553d3a746ecb76d1dc03ec773ac0ee36e764f9a
BugHunter-X <askzenai@gmail.com>
2025-01-19 16:00:06 +0100

    Add incrementCounterSolution.js file

---
+++ incrementCounterSolution.js
@@ -0,0 +1,23 @@
+function incrementCounter(userId) {
+  // Get a reference to the counter document
+  const counterRef = db.collection('counters').doc(userId);
+
+  // Atomically increment the counter using a transaction
+  return db.runTransaction(transaction => {
+    return transaction.get(counterRef)
+      .then(doc => {
+        // Get the current counter value (or 0 if it doesn't exist)
+        let newCount = (doc.exists ? doc.data().count : 0) + 1;
+
+        // If the document doesn't exist, create it
+        if (!doc.exists) {
+          transaction.set(counterRef, { count: newCount });
+        } else {
+          // Update the counter document
+          transaction.update(counterRef, { count: newCount });
+        }
+
+        return newCount;
+      });
+  });
+}
